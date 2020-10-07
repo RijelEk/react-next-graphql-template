a3c560d02afcbc25075ef86387a13d88d26795cd
Jaroslaw <yaroslav@xuver-services.com>
2020-10-07 15:40:55 +0300

    Register page

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,14 @@
+import { Divider } from '@chakra-ui/core';
+import React from 'react';
+
+interface WrapperProps{
+
+}
+
+export const Wrapper: React.FC<WrapperProps> = ({}) =>{
+    return(
+        <div>
+            Wrapper
+        </div>
+    )
+};
---
+++ src/pages/register.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Formik, Form} from 'formik';
+import { FormControl, Input, FormLabel } from "@chakra-ui/core";
+
+interface registerProps{
+
+}
+
+const Register: React.FC<registerProps> = ({}) =>{
+    return(
+        <Formik 
+        initialValues={{username:"", password:""}}
+        onSubmit={(values) =>{
+            console.log(values)
+        }}
+        >
+            {(values, handleChange)=>(
+                <Form>
+                   <FormControl >
+                        <FormLabel htmlFor="username">Username</FormLabel>
+                        <Input  value={values.username} onChange={handleChange} id="username" placeholder="username" />
+                        {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+                    </FormControl>
+                </Form>
+            )}
+        </Formik>
+    );
+}
+
+export default Register;

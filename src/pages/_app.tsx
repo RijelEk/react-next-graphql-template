df802ed9b8e0d9fdd3474ef77cf5a1fc570e3b05
Jaroslaw <yaroslav@xuver-services.com>
2020-10-08 17:42:41 +0300

    Use custom mutation hook to call register action

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,13 @@ import { createClient, Provider } from 'urql';
 import theme from '../theme'
 
 const client = createClient({ 
-  url: 'https://localhost:4000/graphql',
+  url: 'http://localhost:4000/graphql',
   fetchOptions:{
     credentials:"include"
   } 
 });
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: any) {
   return (
     <Provider value={client}>
       <ThemeProvider theme={theme}>

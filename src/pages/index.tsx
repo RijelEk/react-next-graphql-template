55ff11cf946bda11e44cf4e6bf2f87877305b911
Jaroslaw <yaroslav@xuver-services.com>
2020-10-26 18:12:40 +0200

    Fetch posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import {NavBar }from "../components/navBar";
 import {withUrqlClient} from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
+const Index = () => {
+    const [{data}] = usePostsQuery();
+    return (
     <>
 <NavBar/>
 <div>Hello there!</div>
+{!data ? null: data.posts.map((el)=>(
+    <div key={el.id}>{el.title}</div>
+))}
 </>
-);
+)};
 
 export default withUrqlClient(createUrqlClient, {ssr:true})(Index);
